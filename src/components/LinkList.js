6c619047ee3ea01618d3671e2c311940a4520f0b
Rose Sawyer <rosesawyer@Roses-MacBook-Air.local>
2020-03-03 13:54:51 -0500

    voting subscription added

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -47,6 +47,33 @@ const NEW_LINKS_SUBSCRIPTION = gql`
   }
   `
 
+  const NEW_VOTES_SUBSCRIPTION = gql`
+    subscription{
+      newVote{
+        id
+        link{
+          id
+          url
+          description
+          createdAt
+          postedBy {
+            id
+            name
+          }
+          votes {
+            id
+            user {
+              id
+            }
+          }
+        }
+        user {
+          id
+        }
+      }
+    }
+  `
+
 class LinkList extends Component {
     
     _updateCacheAfterVote = (store, createVote, linkId) => {
@@ -77,6 +104,12 @@ class LinkList extends Component {
           }
         })
     }
+
+    _subscribeToNewVotes = subscribeToMore => {
+      subscribeToMore({
+        document: NEW_VOTES_SUBSCRIPTION
+      })
+    }
     
     render() {
       
@@ -88,6 +121,7 @@ class LinkList extends Component {
           if (error) return <div>Error</div>
 
           this._subscribeToNewLinks(subscribeToMore)
+          this._subscribeToNewVotes(subscribeToMore)
     
           const linksToRender = data.feed.links
     
